da043e6131f4ee6c7276f1b04513af9574691bd7
Alexander Ross <alex@aross.se>
2018-11-19 15:08:40 +0100

    Remove unused test

--- spec/standout/capybara_spec.rb
+++ spec/standout/capybara_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Standout::Capybara do
   it "has a version number" do
     expect(Standout::Capybara::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
