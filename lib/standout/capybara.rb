6c0a01941c590ceee2e7a89004bfcf6815a9bab2
Alexander Ross <alex@aross.se>
2020-04-13 13:45:34 +0200

    Add github workflow to publish gem

--- lib/standout/capybara.rb
+++ lib/standout/capybara.rb
@@ -1,18 +1,20 @@
-require "standout/capybara/version"
-require "capybara"
-require "capybara-screenshot"
-require "selenium-webdriver"
-require "chromedriver-helper"
-require "standout/capybara/drivers/base"
-require "standout/capybara/drivers/chrome"
-require "standout/capybara/drivers/nanobox_chrome"
-require "standout/capybara/drivers/headless_chrome"
+# frozen_string_literal: true
+
+require 'standout/capybara/version'
+require 'capybara'
+require 'capybara-screenshot'
+require 'selenium-webdriver'
+require 'chromedriver-helper'
+require 'standout/capybara/drivers/base'
+require 'standout/capybara/drivers/chrome'
+require 'standout/capybara/drivers/nanobox_chrome'
+require 'standout/capybara/drivers/headless_chrome'
 
 module Standout
   module Capybara
     def self.setup_driver
-      driver_name = ENV["CAPYBARA"]
-      is_using_nanobox = `whoami`.chomp == "gonano"
+      driver_name = ENV['CAPYBARA']
+      is_using_nanobox = `whoami`.chomp == 'gonano'
       fallback_driver = is_using_nanobox ? :nanobox_chrome : :headless_chrome
       driver_name ||= fallback_driver
       driver_name = driver_name.to_sym
